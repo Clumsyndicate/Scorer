0becf292a4db9585484da05b840f0057d2704e89
Clumsyndicate <johnson.zhou.sh@icloud.com>
2018-02-06 10:14:10 +0800

    Initial Commit

---
+++ Scorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scorer
+//
+//  Created by Johnson Zhou on 06/02/2018.
+//  Copyright © 2018 Johnson Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
